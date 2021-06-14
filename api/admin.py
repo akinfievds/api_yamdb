cc18688e3ade01b43cbc164825fb5e2aa6b4aab3
Roman Malkov <rommalkov@example.com>
2021-06-14 12:26:15 +0300

    Расширил классы Category, Genre, Title в models.py и admin.py

--- api/admin.py
+++ api/admin.py
@@ -5,14 +5,23 @@ from .models import Category, Genre, Title
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name',)
     empty_value_display = '-пусто-'
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'slug')
+    search_fields = ('name',)
+    list_filter = ('name',)
     empty_value_display = '-пусто-'
 
 
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'year', 'genre', 'category', 'description')
+    search_fields = ('name', 'year',)
+    list_filter = ('year',)
     empty_value_display = '-пусто-'
