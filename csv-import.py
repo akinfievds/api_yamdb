5b04e812dedd31a6efc156e9f93e007e811fe87e
Дмитрий <akinfievds@gmail.com>
2021-06-15 17:29:19 +0300

    Файл импорта готов (4)

--- csv-import.py
+++ csv-import.py
@@ -2,17 +2,9 @@ import argparse
 import csv
 import sqlite3
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--db', help='Path to database')
-    parser.add_argument('--csv', help='Path to csv file')
-    parser.add_argument('--table', help='SQL Table name')
-    args = parser.parse_args()
 
-    connection = sqlite3.connect(args.db)
-    cursor = connection.cursor()
-
-    with open(args.csv) as file:
+def copy_csv_to_db(path_to_csv, table_name):
+    with open(path_to_csv) as file:
         line = 0
         for row in csv.reader(file):
             line += 1
@@ -23,35 +15,58 @@ if __name__ == '__main__':
                     values += ', ?'
             insert_records = (
                 'INSERT INTO {table} ({headers}) VALUES ({values})'
-            ).format(table=args.table, headers=headers, values=values)
-            try:
-                cursor.execute(insert_records, row)
-            except sqlite3.IntegrityError as err1:
-                FORMAT = (
-                    '\033[1;31m' + 'Похоже на то, что в строках с этим pk '
-                    'какие-то данные уже есть.' + '\033[0m' + '\nОшибка:'
-                )
-                print(FORMAT, err1)
-                break
-            except sqlite3.OperationalError as err2:
-                FORMAT = (
-                    '\033[1;31m' + 'Похоже на то, что что-то '
-                    'не так с названиями или количеством столбцов '
-                    'в CSV или DB.' + '\033[0m' + '\nОшибка:'
-                )
-                print(FORMAT, err2)
-                break
+            ).format(table=table_name, headers=headers, values=values)
+            if line != 1:
+                try:
+                    cursor.execute(insert_records, row)
+                except sqlite3.IntegrityError as err1:
+                    msg1 = (
+                        '\033[1;31m' + 'Похоже с данными что-то не так '
+                        'или таблица уже заполнена.' + '\033[0m' + '\nОшибка:'
+                    )
+                    print(msg1, err1)
+                    error = True
+                    break
+                except sqlite3.OperationalError as err2:
+                    msg2 = (
+                        '\033[1;31m' + 'Похоже на то, что что-то '
+                        'не так с названиями или количеством столбцов '
+                        'в CSV или DB.' + '\033[0m' + '\nОшибка:'
+                    )
+                    print(msg2, err2)
+                    error = True
+                    break
+    return error
+
+
+def check_result(path_to_db, table_name, error=None):
+    select_all = 'SELECT * FROM {table}'.format(table=args.table)
+    db_echo = cursor.execute(select_all).fetchall()
+    if not error and db_echo:
+        print(
+            'В таблицу {table} базы данных {db} записаны '
+            'следующие строки:'.format(
+                table=table_name,
+                db=path_to_db
+            )
+        )
+        for line in cursor.execute(select_all).fetchall():
+            print('\033[32m' + 'Записана строка: ' + '\033[0m', line)
+        print('\n Итого в базу записано {qty} строк'.format(qty=len(db_echo)))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--db', help='Path to database')
+    parser.add_argument('--csv', help='Path to csv file')
+    parser.add_argument('--table', help='SQL Table name')
+    args = parser.parse_args()
+
+    connection = sqlite3.connect(args.db)
+    cursor = connection.cursor()
+
+    result = copy_csv_to_db(path_to_csv=args.csv, table_name=args.table)
+    check_result(path_to_db=args.db, table_name=args.table, error=result)
 
     connection.commit()
-
-    print(
-        'В таблицу {table} базы данных {db} записаны следующие строки:'.format(
-            table=args.table,
-            db=args.db
-        )
-    )
-    select_all = 'SELECT * FROM {table}'.format(table=args.table)
-    for line in cursor.execute(select_all).fetchall():
-        print('\033[32m' + 'Записана строка: ' + '\033[0m', line)
-
     connection.close()
