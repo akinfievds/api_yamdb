c997942d0c03b0060380dff0696c16e3e4bef0ea
Дмитрий <akinfievds@gmail.com>
2021-06-15 19:21:54 +0300

    Бета версия для сериализатора и эндпоинта для GET-запроса модели Review

--- api/urls.py
+++ api/urls.py
@@ -1,8 +1,30 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
+from api.views import ReviewViewSet
+
+router_v1 = DefaultRouter()
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/reviews',
+    ReviewViewSet,
+    basename='reviews'
+)
+
 urlpatterns = [
-    path('auth/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path(
+        'auth/token/',
+        TokenObtainPairView.as_view(),
+        name='token_obtain_pair'
+    ),
+    path(
+        'auth/token/refresh/',
+        TokenRefreshView.as_view(),
+        name='token_refresh'
+    ),
+    path(
+        '',
+        include(router_v1.urls)
+    )
 ]
