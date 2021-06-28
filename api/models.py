5a37024c9373ffba32061d685a9a45e08f46ecf9
Дмитрий <akinfievds@gmail.com>
2021-06-28 10:15:57 +0300

    Подготовка к ревью 5

--- api/models.py
+++ api/models.py
@@ -29,7 +29,7 @@ class User(AbstractUser):
         verbose_name='Информация о пользователе'
     )
     role = models.CharField(
-        max_length=20,
+        max_length=40,
         choices=UserRole.choices,
         default=UserRole.USER
     )
